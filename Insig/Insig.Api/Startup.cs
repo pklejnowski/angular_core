06e4b21e83d76111b759d6f04d7585cb2fddbedc
Piotr Klejnowski <klejnowski.piotr@gmail.com>
2018-05-27 13:55:40 +0200

    udpated startup

--- Insig/Insig.Api/Startup.cs
+++ Insig/Insig.Api/Startup.cs
@@ -7,12 +7,17 @@ namespace Insig.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -26,7 +31,7 @@ namespace Insig.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(b => b.WithOrigins("http://localhost:5000").AllowAnyHeader().AllowAnyMethod());
+            app.UseCors(b => b.WithOrigins(Configuration["AppUrls:ClientUrl"]).AllowAnyHeader().AllowAnyMethod());
 
             app.UseMvc();
         }
