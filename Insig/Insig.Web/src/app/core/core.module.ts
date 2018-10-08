a77f3b02cf153d7a35587068d6de856e8de825d2
Piotr Klejnowski <klejnowski.piotr@gmail.com>
2018-10-08 23:00:39 +0200

    Added toast

--- Insig/Insig.Web/src/app/core/core.module.ts
+++ Insig/Insig.Web/src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { ToastrModule } from "ngx-toastr";
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
   declarations: [],
   providers: []
