890fc9128323ce7f1a71c92bf68dfdd8a1cceecd
Piotr Klejnowski <klejnowski.piotr@gmail.com>
2018-11-01 12:07:52 +0100

    updated angular from 6 to 7 version

--- Insig/Insig.Web/src/app/core/interceptors/toast-http-interceptor.ts
+++ Insig/Insig.Web/src/app/core/interceptors/toast-http-interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
 @Injectable()
@@ -13,7 +13,7 @@ export class ToastHttpInterceptor implements HttpInterceptor {
         return next.handle(req).pipe(
             catchError((err) => {
                 this.toastr.error("Operation failed.", "Error");
-                return Observable.throw(err);
+                return throwError(err);
             })
         );
     }
