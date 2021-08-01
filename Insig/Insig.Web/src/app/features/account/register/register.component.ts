d1381a4802688be9ce36a1cf240819acb283157c
Piotr Klejnowski <divao@live.com>
2021-08-01 14:39:51 +0200

    eslint fixes

--- Insig/Insig.Web/src/app/features/account/register/register.component.ts
+++ Insig/Insig.Web/src/app/features/account/register/register.component.ts
@@ -4,33 +4,36 @@ import { Router } from "@angular/router";
 import { AuthService } from "@app/auth";
 import { ToastrService } from "ngx-toastr";
 
+import { PasswordValidator } from "./password-strength.validator";
+
 @Component({
     selector: "app-register",
     templateUrl: "./register.component.html"
 })
 export class RegisterComponent {
-
-    registerForm = this.fb.group({
+    registerForm = this._fb.group({
         email: ["", [Validators.required, Validators.email]],
         phoneNumber: ["", [Validators.required, Validators.minLength(6)]],
-        password: ["", [Validators.required, Validators.minLength(6)]],
+        password: ["", [PasswordValidator.strength]],
         confirmPassword: ["", [Validators.required]]
+    }, {
+        validators: PasswordValidator.confirmed("password", "confirmPassword")
     });
 
-    constructor(private fb: FormBuilder, private authService: AuthService, private toastr: ToastrService, private router: Router) { }
+    constructor(private _fb: FormBuilder, private _authService: AuthService, private _toastr: ToastrService, private _router: Router) { }
 
     register(): void {
         if (this.registerForm.valid) {
-            this.authService.register({
+            this._authService.register({
                 email: this.registerForm.value.email,
-                phoneNumber: "+48" + this.registerForm.value.phoneNumber,
+                phoneNumber: `+48${this.registerForm.value.phoneNumber as string}`,
                 password: this.registerForm.value.password,
-                redirectUrl: appConfig.clientUrl + "/login"
+                redirectUrl: `${appConfig.clientUrl}/login`
             }).subscribe(_ => {
-                this.toastr.success("A verification link has been sent to your email account", "Thank you for registering");
-                this.router.navigate(["login"]);
+                this._toastr.success("A verification link has been sent to your email account", "Thank you for registering");
+                this._router.navigate(["login"]);
             }, error => {
-                this.toastr.error(error.error[0].description);
+                this._toastr.error(error.error[0].description);
             });
         }
     }
--- Insig/Insig.Web/src/app/shared/validation-feedback/validation-feedback.component.ts
+++ Insig/Insig.Web/src/app/shared/validation-feedback/validation-feedback.component.ts
@@ -1,33 +1,41 @@
 import { Component, Input, OnInit, Optional } from "@angular/core";
 import { AbstractControl, FormGroupDirective } from "@angular/forms";
 
+interface MinLength {
+    requiredLength: number;
+}
+
 @Component({
     selector: "app-validation-feedback",
     templateUrl: "./validation-feedback.component.html"
 })
 export class ValidationFeedbackComponent implements OnInit {
-
     @Input() control: Nullable<AbstractControl>;
     @Input() controlName: Nullable<string>;
 
-    constructor(@Optional() private formGroup: FormGroupDirective) { }
+    private _validatorErrors: { [key: string]: string };
+
+    constructor(@Optional() private _formGroup: FormGroupDirective) { }
 
     getValidationError(): string {
-        if (this.control?.hasError("required")) {
-            return "Field is required.";
-        } else if (this.control?.hasError("minlength")) {
-            return `Field has to have at least ${this.control.getError("minlength").requiredLength} characters.`;
-        } else if (this.control?.hasError("email")) {
-            return "E-mail format is incorrect.";
+        if (this.control) {
+            this._validatorErrors = {
+                required: "Field is required.",
+                minlength: `Field has to have at least ${(this.control.getError("minlength") as MinLength)?.requiredLength} characters.`,
+                email: "E-mail format is incorrect."
+            };
+
+            return this._validatorErrors[Object.keys((this.control.errors as any))[0]];
         }
+
         return "";
     }
 
     ngOnInit(): void {
         if (!this.control && !this.controlName) {
             throw new Error("Validation Feedback must have [control] or [controlName] inputs");
-        } else if (this.controlName && this.formGroup) {
-            this.control = this.formGroup.form.get(this.controlName);
+        } else if (this.controlName && this._formGroup) {
+            this.control = this._formGroup.form.get(this.controlName);
         }
     }
 }
